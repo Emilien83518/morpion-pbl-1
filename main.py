b7c7d4969d09c1d0249c4889c11157e22d511bb8
Emilien83518 <emilien.amon@esme.fr>
2026-02-18 11:28:12 +0100

    main

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import sys
 from PyQt5.QtGui import QIcon
 # Main class for the PyQt5 application
 from PyQt5.QtWidgets import QApplication
-# Import the main application window
-from view.quiz_view import QuizView
-app = QApplication(sys.argv) # Creates the application
+# Import the main application window from morpion.py
+from morpion import QuizView
+
+app = QApplication(sys.argv)  # Creates the application
 app.setWindowIcon(QIcon("resources/quizicon.png"))
-window = QuizView() # Creates the window
-window.show() # Displays the window
-sys.exit(app.exec_()) # Starts the application loop and exits cleanly
+window = QuizView()  # Creates the window
+window.show()  # Displays the window
+sys.exit(app.exec_())  # Starts the application loop and exits cleanly
--- morpion.py
+++ morpion.py
@@ -1,39 +1,31 @@
 
-from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QLayout, QHBoxLayout, QRadioButton, QPushButton
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QLayout, QHBoxLayout, QPushButton, QGridLayout
 from PyQt5.QtCore import Qt
 class QuizView( QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("QuizMaster")
-        self.resize(400, 300)
-        self.setStyleSheet("background-color: lightblue;")
+        self.setWindowTitle("Morpion")
+        self.resize(1920, 1080)
+        self.setStyleSheet("background-color: darkred;")
         self.setMinimumSize(800, 600)
-        self.question_label = QLabel("What is the capital of France?", self)
-        #self.question_label.setAlignment(Qt.AlignCenter) # Centre the text
-        self.question_label.move(50, 100)
+        self.question_label = QLabel("TIC TAC TOE GAME", self)
+        self.question_label.setAlignment(Qt.AlignCenter) # Centre the text
+        self.question_label.move(50, 50)
         # ----- Main layout -----
         main_layout = QVBoxLayout()
         main_layout.addWidget(self.question_label)
-        #---- answer layout-----
-        answer_layout = QVBoxLayout()
-        answer_1= QRadioButton("Paris")
-        answer_2= QRadioButton('Lyon')
-        answer_3= QRadioButton('Marseille')
-        answer_4= QRadioButton('Bordeaux')
-        answer_layout.addWidget(answer_1)
-        answer_layout.addWidget(answer_2)
-        answer_layout.addWidget(answer_3)
-        answer_layout.addWidget(answer_4)
-        #------nav_layout----
-        nav_layout = QHBoxLayout()
-        nav_prev=QPushButton("previous")
-        nav_next=QPushButton('next')
-        nav_layout.addWidget(nav_prev)
-        nav_layout.addWidget(nav_next)
+      
+        #------board_layout----
+        board_layout = QGridLayout()
+        for row in range(3):
+            for col in range(3):
+                button = QPushButton(f"Button {row},{col}")
+                button.setFixedSize(200,200)
+                board_layout.addWidget(button, row, col)
+        board_layout.setHorizontalSpacing(10)
 
-
-        main_layout.addLayout(answer_layout)
-        main_layout.addLayout(nav_layout)
+        
+        main_layout.addLayout(board_layout)
 
         self.setLayout(main_layout)
 
