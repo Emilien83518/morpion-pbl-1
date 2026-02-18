d5611c7930cb8fe7ef03959ce7bb061c5c1c2a31
etienne <etienne.hawk@gamil.com>
2026-02-18 10:21:52 +0100

    base

--- main.py
+++ main.py
@@ -39,4 +39,6 @@ while Me==0 and Ai==0:
     elif board[0][0]==board[0][1]==board[0][2]=="O" or board[1][0]==board[1][1]==board[1][2]=="O" or board[2][0]==board[2][1]==board[2][2]=="O" or board[0][0]==board[1][0]==board[2][0]=="O" or board[0][1]==board[1][1]==board[2][1]=="O" or board[0][2]==board[1][2]==board[2][2]=="O" or board[0][0]==board[1][1]==board[2][2]=="O" or board[0][2]==board[1][1]==board[2][0]=="O":
         Ai = 1
         print("you lost")
+def thomaspucamere():
+    print("(-_-)")
 
