a4a81c79e3501726e2b86eca74c32e80c2dfc810
Gaspard-f0urc <gaspard.fourcassie@esme.fr>
2026-02-23 15:22:38 +0100

    def terminal 2

--- main.py
+++ main.py
@@ -6,13 +6,10 @@
 #def player: determines whose turn it is in any given game state (either max or min)
 #def action: gives all the possible actions we can take in that state  
 #def results: tells what the new state of the game will be after the action 
-board=[['X','X','O'],
-       ['','O',''],
+board=[['X','X',''],
+       ['O','O','O'],
        ['O','X','O']]
 def terminal(board):
-    for r in board:
-        if '' in r: 
-            return False 
     for row in board:
         if row[0]==row[1]==row[2] and row[0]!='':
             return True 
@@ -23,5 +20,9 @@ def terminal(board):
             return True    
     if board[0][2]==board[1][1]==board[2][0] and board[0][2]!='':
             return True 
+    for item in board:
+        if '' in item:
+            return False
     return True
+                
 print(terminal(board))
