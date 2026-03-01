cce1928474f34c400475f3550ee342cca4e61df2
Gaspard-f0urc <gaspard.fourcassie@esme.fr>
2026-03-01 16:13:25 +0100

    Final

--- main.py
+++ main.py
@@ -7,24 +7,88 @@
 #def action: gives all the possible actions we can take in that state  
 #def results: tells what the new state of the game will be after the action 
 
-def terminal(board):
-    for row in board:
-        if row[0]==row[1]==row[2] and row[0]!='':
-            return True 
-    for col in range(3):
-        if board[0][col]==board[1][col]==board[2][col] and board[0][col]!='':
-            return True     
-    if board[0][0]==board[1][1]==board[2][2] and board[0][0]!='':
-            return True    
-    if board[0][2]==board[1][1]==board[2][0] and board[0][2]!='':
-            return True 
-    for item in board:
-        if '' in item:
-            return False
-    return True
-def value(board):
-    if board[0][0]==board[0][1]==board[0][2]=="X" or board[1][0]==board[1][1]==board[1][2]=="X" or board[2][0]==board[2][1]==board[2][2]=="X" or board[0][0]==board[1][0]==board[2][0]=="X" or board[0][1]==board[1][1]==board[2][1]=="X" or board[0][2]==board[1][2]==board[2][2]=="X" or board[0][0]==board[1][1]==board[2][2]=="X" or board[0][2]==board[1][1]==board[2][0]=="X":
-        return 1 
-    elif board[0][0]==board[0][1]==board[0][2]=="O" or board[1][0]==board[1][1]==board[1][2]=="O" or board[2][0]==board[2][1]==board[2][2]=="O" or board[0][0]==board[1][0]==board[2][0]=="O" or board[0][1]==board[1][1]==board[2][1]=="O" or board[0][2]==board[1][2]==board[2][2]=="O" or board[0][0]==board[1][1]==board[2][2]=="O" or board[0][2]==board[1][1]==board[2][0]=="O":
-        return -1
-    return 0
+import copy
+
+def Terminal(b, p):
+    wins = [
+        [b[0][0], b[0][1], b[0][2]], [b[1][0], b[1][1], b[1][2]], [b[2][0], b[2][1], b[2][2]],
+        [b[0][0], b[1][0], b[2][0]], [b[0][1], b[1][1], b[2][1]], [b[0][2], b[1][2], b[2][2]],
+        [b[0][0], b[1][1], b[2][2]], [b[0][2], b[1][1], b[2][0]]
+    ]
+    return [p, p, p] in wins
+
+def minimax(board_state, is_maximizing):
+    # Terminal states
+    if Terminal(board_state, "O"): return 10
+    if Terminal(board_state, "X"): return -10
+    if not any("" in row for row in board_state): return 0
+
+    if is_maximizing:
+        best_score = -float('inf')
+        for r in range(3):
+            for c in range(3):
+                if board_state[r][c] == "":
+                    board_state[r][c] = "O"
+                    score = minimax(board_state, False)
+                    board_state[r][c] = ""
+                    best_score = max(score, best_score)
+        return best_score
+    else:
+        best_score = float('inf')
+        for r in range(3):
+            for c in range(3):
+                if board_state[r][c] == "":
+                    board_state[r][c] = "X"
+                    score = minimax(board_state, True)
+                    board_state[r][c] = ""
+                    best_score = min(score, best_score)
+        return best_score
+s=True
+board=[["","",""],["","",""],["","",""]]
+Me=0
+Ai=0
+
+while Me==0 and Ai==0:
+    while s == True:
+        row = input("row (0-2): ")
+        column = input("column (0-2): ")
+        if board[int(row)][int(column)] == "":
+            board[int(row)][int(column)] = "X"
+            s = False
+        else:
+            s = True
+    
+    if Terminal(board, "X"):
+        Me = 1
+        print("you won")
+        break
+    if not any("" in row for row in board):
+        print("draw")
+        break
+
+  
+    best_val = -float('inf')
+    best_move = (-1, -1)
+    
+    for r in range(3):
+        for c in range(3):
+            if board[r][c] == "":
+                board[r][c] = "O"
+                move_val = minimax(board, False)
+                board[r][c] = ""
+                if move_val > best_val:
+                    best_move = (r, c)
+                    best_val = move_val
+    
+    if best_move != (-1, -1):
+        board[best_move[0]][best_move[1]] = "O"
+        print(f"AI plays at: {best_move[0]} {best_move[1]}")
+    
+    s = True 
+
+    for row_list in board:
+        print(row_list)
+
+    if Terminal(board, "O"):
+        Ai = 1
+        print("you lost")
